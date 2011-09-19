af4adc20c0b123f52575cdc45832542349d8e24e
Michael Westrich <westrich.michael@googlemail.com>
2011-09-19 15:33:55 +0200

    Source code checkin

---
+++ privateDelicious.js
@@ -0,0 +1,3 @@
+//Checks "Make Private" by default on "Save a Bookmark on Delicious" page.
+if (!document.getElementById("savePrivate").checked)
+	document.getElementById("savePrivate").checked=true;
